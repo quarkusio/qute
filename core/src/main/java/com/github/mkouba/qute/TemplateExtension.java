fee4f098a85b61f08abf0a9bda7a0a25caff58a8
Martin Kouba <mkouba@redhat.com>
2019-07-15 14:15:57 +0200

    Update TemplateExtension javadoc

--- core/src/main/java/com/github/mkouba/qute/TemplateExtension.java
+++ core/src/main/java/com/github/mkouba/qute/TemplateExtension.java
@@ -7,17 +7,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
- * A value resolver is automatically generated for template extension methods.
- * 
+ * A value resolver is automatically generated for a template extension method.
+ * <p>
  * The method must be static, must not return {@code void} and must accept at least one parameter. The class of the first
- * parameter is used to match the base object. 
- * 
- * 
- * The method name is used to match the property name.
+ * parameter is used to match the base object.
+ * <p>
+ * By default, the method name is used to match the property name. However, it is possible to specify the matching name with
+ * {@link #matchName()}. A special constant - {@link #ANY} - may be used to specify that the extension method matches any name.
+ * In that case, the method must declare at least two parameters and the second parameter must be a string.
  * 
  * <pre>
  * {@literal @}TemplateExtension
  * static BigDecimal discountedPrice(Item item) {
+ *    // this method matches {item.discountedPrice}
  *    return item.getPrice().multiply(new BigDecimal("0.9"));
  * }
  * </pre>
@@ -25,9 +27,9 @@ import java.lang.annotation.Target;
 @Retention(RUNTIME)
 @Target(METHOD)
 public @interface TemplateExtension {
-    
+
     static final String ANY = "*";
-    
+
     String matchName() default "";
 
 }
